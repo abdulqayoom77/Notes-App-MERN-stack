574555fb8dc133788fbe11aa984fc5482230eb17
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2024-12-06 21:50:24 +0500

    initial

--- backend/index.js
+++ backend/index.js
@@ -346,8 +346,8 @@ app.get("/search-notes/", authenticateToken, async (req, res) => {
 });
 
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running on port", process.env.PORT || 8000);
 });
 
 export default app;
