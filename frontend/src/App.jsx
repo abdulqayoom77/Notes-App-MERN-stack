508e855f0ef78d591bad0f84c06b974fe4850b49
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2024-12-07 14:52:49 +0500

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
